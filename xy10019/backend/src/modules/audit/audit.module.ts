928cc68aff7ca32d0a55d6afbce631d3af14e782
Solo Publisher <solo-publisher@example.invalid>
2026-05-11 13:16:43 +0800

    Publish xy10019

--- xy10019/backend/src/modules/audit/audit.module.ts
+++ xy10019/backend/src/modules/audit/audit.module.ts
@@ -2,9 +2,11 @@ import { Module, Global } from '@nestjs/common';
 import { AuditService } from './audit.service';
 import { AuditController } from './audit.controller';
 import { RollbackService } from './rollback.service';
+import { AuthModule } from '../auth/auth.module';
 
 @Global()
 @Module({
+  imports: [AuthModule],
   controllers: [AuditController],
   providers: [AuditService, RollbackService],
   exports: [AuditService, RollbackService],
