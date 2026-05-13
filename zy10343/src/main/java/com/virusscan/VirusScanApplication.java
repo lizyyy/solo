0eac25e7fe07a1f9a7df12711548413428972877
lzy <lizhiyuan01@zuoyebang.com>
2026-05-14 03:59:24 +0700

    Publish zy10343

---
+++ zy10343/src/main/java/com/virusscan/VirusScanApplication.java
@@ -0,0 +1,11 @@
+package com.virusscan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VirusScanApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(VirusScanApplication.class, args);
+    }
+}
---
+++ zy10343/src/main/java/com/virusscan/enums/TaskStatus.java
@@ -0,0 +1,12 @@
+package com.virusscan.enums;
+
+public enum TaskStatus {
+    PENDING,
+    SCANNING,
+    CLEAN,
+    INFECTED,
+    QUARANTINED,
+    RELEASED,
+    FAILED,
+    CANCELLED
+}
